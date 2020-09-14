b0808339e53e8f4e5a6435f150f1e262dd60f586
Victor Popkov <victor@popkov.me>
2020-09-14 17:34:11 +0300

    Bump version from 0.7.0-alpha to 0.7.0

--- scripts/autojoin.lua
+++ scripts/autojoin.lua
@@ -13,7 +13,7 @@
 -- @author Victor Popkov
 -- @copyright 2019
 -- @license MIT
--- @release 0.7.0-alpha
+-- @release 0.7.0
 ----
 require "autojoin/constants"
 require "class"
--- scripts/autojoin/utils/chain.lua
+++ scripts/autojoin/utils/chain.lua
@@ -9,7 +9,7 @@
 -- @author Victor Popkov
 -- @copyright 2019
 -- @license MIT
--- @release 0.7.0-alpha
+-- @release 0.7.0
 ----
 local Chain = {}
 
