8b9454efbc7cff484ec1e475c4923171aa6cb5cb
Victor Popkov <victor@popkov.me>
2020-10-05 13:52:34 +0300

    Improve some existing status icons

--- modinfo.lua
+++ modinfo.lua
@@ -8,6 +8,7 @@ description = [[Version: ]] .. version .. "\n\n" ..
     [[the second - rejoin the last server.]] .. "\n\n" ..
 
     [[v]] .. version .. [[:]] .. "\n" ..
+    [[- Improved some existing status icons]] .. "\n" ..
     [[- Improved the "Dev Tools" mod submenu]] .. "\n" ..
     [[- Refactored modinfo]]
 author = "Demonblink"
