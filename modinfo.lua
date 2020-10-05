e6294162e4fb16afc7e3353fbbf0896dc5b8e9f8
Victor Popkov <victor@popkov.me>
2020-10-05 12:16:51 +0300

    Refactor modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 name = "Auto-Join"
 version = "0.8.0-alpha"
 description = [[Version: ]] .. version .. "\n\n" ..
+
     [[Adds "Auto-Join" and "Rejoin" buttons.]] .. "\n\n" ..
-    [[The first one allows continuous reconnections to the selected server until joining, and the second - rejoin the last server.]] .. "\n\n" ..
+
+    [[The first one allows continuous reconnections to the selected server until joining, and ]] ..
+    [[the second - rejoin the last server.]] .. "\n\n" ..
+
     [[v]] .. version .. [[:]] .. "\n" ..
-    [[- Added support for "Rejoin" pause screen button]] .. "\n" ..
-    [[- Added support for the last connection status]] .. "\n" ..
-    [[- Added support for the rejoin pause screen button configuration]] .. "\n" ..
-    [[- Changed indicator position configuration]] .. "\n" ..
-    [[- Fixed issue with rejoining a non-master shard]]
+    [[- Refactored modinfo]]
 author = "Demonblink"
 api_version = 10
 forumthread = ""
@@ -31,18 +31,26 @@ if not folder_name:find("workshop-") then
 end
 
 --
--- Helpers
+-- Configuration
 --
 
-local function AddConfig(label, name, options, default, hover)
+local function AddConfig(name, label, hover, options, default)
     return { label = label, name = name, options = options, default = default, hover = hover or "" }
 end
 
-local function AddSection(title)
-    return AddConfig(title, "", { { description = "", data = 0 } }, 0)
+local function AddBooleanConfig(name, label, hover, default)
+    default = default == nil and true or default
+    return AddConfig(name, label, hover, {
+        { description = "Enabled", data = true },
+        { description = "Disabled", data = false },
+    }, default)
 end
 
-local function CreateKeyList()
+local function AddKeyListConfig(name, label, hover, default)
+    if default == nil then
+        default = false
+    end
+
     -- helpers
     local function AddDisabled(t)
         t[#t + 1] = { description = "Disabled", data = false }
@@ -116,83 +124,153 @@ local function CreateKeyList()
     AddNavigationKeys(list)
     AddKeysByName(list, { "Escape", "Pause", "Print" })
 
-    return list
+    return AddConfig(name, label, hover, list, default)
 end
 
---
--- Configuration
---
-
-local key_list = CreateKeyList()
-
-local boolean = {
-    { description = "Enabled", data = true },
-    { description = "Disabled", data = false },
-}
-
-local indicator_padding = {
-    { description = "5", data = 5 },
-    { description = "10", data = 10 },
-    { description = "15", data = 15 },
-    { description = "20", data = 20 },
-}
-
-local indicator_position = {
-    { description = "Top Left", data = 1 },
-    { description = "Top Right", data = 2 },
-    { description = "Bottom Right", data = 3 },
-    { description = "Bottom Left", data = 4 },
-}
-
-local indicator_scale = {
-    { description = "1", data = 1 },
-    { description = "1.1", data = 1.1 },
-    { description = "1.2", data = 1.2 },
-    { description = "1.3", data = 1.3 },
-    { description = "1.4", data = 1.4 },
-    { description = "1.5", data = 1.5 },
-}
-
-local waiting_time = {
-    { description = "5s", data = 5 },
-    { description = "10s", data = 10 },
-    { description = "15s", data = 15 },
-    { description = "20s", data = 20 },
-    { description = "25s", data = 25 },
-    { description = "30s", data = 30 },
-    { description = "35s", data = 35 },
-    { description = "40s", data = 40 },
-    { description = "45s", data = 45 },
-    { description = "50s", data = 50 },
-    { description = "55s", data = 55 },
-    { description = "1m", data = 60 },
-}
-
-local rejoin_initial_wait = {
-    { description = "1s", data = 1 },
-    { description = "2s", data = 2 },
-    { description = "3s", data = 3 },
-    { description = "5s", data = 5 },
-    { description = "10s", data = 10 },
-}
+local function AddSection(title)
+    return AddConfig("", title, nil, { { description = "", data = 0 } }, 0)
+end
 
 configuration_options = {
+    --
+    -- General
+    --
+
     AddSection("General"),
-    AddConfig("Waiting time", "waiting_time", waiting_time, 15, "The time between reconnection attempts"),
+
+    AddConfig(
+        "waiting_time",
+        "Waiting time",
+        "The time between reconnection attempts",
+        {
+            { description = "5s", data = 5 },
+            { description = "10s", data = 10 },
+            { description = "15s", data = 15 },
+            { description = "20s", data = 20 },
+            { description = "25s", data = 25 },
+            { description = "30s", data = 30 },
+            { description = "35s", data = 35 },
+            { description = "40s", data = 40 },
+            { description = "45s", data = 45 },
+            { description = "50s", data = 50 },
+            { description = "55s", data = 55 },
+            { description = "1m", data = 60 },
+        },
+        15
+    ),
+
+    --
+    -- Indicator
+    --
 
     AddSection("Indicator"),
-    AddConfig("Indicator", "indicator", boolean, true, "When enabled, adds a corner indicator on the supported screens"),
-    AddConfig("Indicator position", "indicator_position", indicator_position, 2, "Indicator position on the screen"),
-    AddConfig("Indicator padding", "indicator_padding", indicator_padding, 10, "Indicator padding from the screen edges"),
-    AddConfig("Indicator scale", "indicator_scale", indicator_scale, 1.3, "Indicator scale on the screen"),
+
+    AddBooleanConfig(
+        "indicator",
+        "Indicator",
+        "When enabled, adds a corner indicator on the supported screens"
+    ),
+
+    AddConfig(
+        "indicator_position",
+        "Indicator position",
+        "Indicator position on the screen",
+        {
+            { description = "Top Left", data = 1 },
+            { description = "Top Right", data = 2 },
+            { description = "Bottom Right", data = 3 },
+            { description = "Bottom Left", data = 4 },
+        },
+        2
+    ),
+
+    AddConfig(
+        "indicator_padding",
+        "Indicator padding",
+        "Indicator padding from the screen edges",
+        {
+            { description = "5", data = 5 },
+            { description = "10", data = 10 },
+            { description = "15", data = 15 },
+            { description = "20", data = 20 },
+        },
+        10
+    ),
+
+    AddConfig(
+        "indicator_scale",
+        "Indicator scale",
+        "Indicator scale on the screen",
+        {
+            { description = "1", data = 1 },
+            { description = "1.1", data = 1.1 },
+            { description = "1.2", data = 1.2 },
+            { description = "1.3", data = 1.3 },
+            { description = "1.4", data = 1.4 },
+            { description = "1.5", data = 1.5 },
+        },
+        1.3
+    ),
+
+    --
+    -- Rejoin
+    --
 
     AddSection("Rejoin"),
-    AddConfig("Rejoin key", "key_rejoin", key_list, "KEY_CTRL", "Key used for toggling the rejoin functionality.\nAvailable on the main and pause screens"),
-    AddConfig("Rejoin initial wait", "rejoin_initial_wait", rejoin_initial_wait, 3, "Initial wait in seconds to retrieve a list of servers before rejoining.\nChange based on your network speed"),
-    AddConfig("Rejoin main screen button", "rejoin_main_screen_button", boolean, true, 'When enabled, adds "Rejoin" button in the multiplayer main screen.\nOn Windows, replaces the last "Games" button'),
-    AddConfig("Rejoin pause screen button", "rejoin_pause_screen_button", boolean, true, 'When enabled, adds "Rejoin" button in the pause screen.\nReplaces "Disconnect" while holding the rejoin key'),
+
+    AddKeyListConfig(
+        "key_rejoin",
+        "Rejoin key",
+        "Key used for toggling the rejoin functionality.\nAvailable on the main and pause screens",
+        "KEY_CTRL"
+    ),
+
+    AddConfig(
+        "rejoin_initial_wait",
+        "Rejoin initial wait",
+        [[Initial wait in seconds to retrieve a list of servers before rejoining.]] .. "\n" ..
+            [[Change based on your network speed]],
+        {
+            { description = "1s", data = 1 },
+            { description = "2s", data = 2 },
+            { description = "3s", data = 3 },
+            { description = "5s", data = 5 },
+            { description = "10s", data = 10 },
+        },
+        3
+    ),
+
+    AddBooleanConfig(
+        "rejoin_main_screen_button",
+        "Rejoin main screen button",
+        [[When enabled, adds "Rejoin" button in the multiplayer main screen.]] .. "\n" ..
+            [[On Windows, replaces the last "Games" button']]
+    ),
+
+    AddBooleanConfig(
+        "rejoin_pause_screen_button",
+        "Rejoin pause screen button",
+        [[When enabled, adds "Rejoin" button in the pause screen.]] .. "\n" ..
+            [[Replaces "Disconnect" while holding the rejoin key]]
+    ),
+
+    --
+    -- Other
+    --
 
     AddSection("Other"),
-    AddConfig("Hide changelog", "hide_changelog", boolean, true, "When enabled, hides the changelog in the mod description.\nMods should be reloaded to take effect"),
-    AddConfig("Debug", "debug", boolean, false, "When enabled, displays debug data in the console.\nUsed mainly for development"),
+
+    AddBooleanConfig(
+        "hide_changelog",
+        "Hide changelog",
+        [[When enabled, hides the changelog in the mod description.]] .. "\n" ..
+            [[Mods should be reloaded to take effect]]
+    ),
+
+    AddBooleanConfig(
+        "debug",
+        "Debug",
+        "When enabled, displays debug data in the console.\nUsed mainly for development",
+        false
+    ),
 }
